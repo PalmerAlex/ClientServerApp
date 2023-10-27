65eb8943048b47a01489fe41aba1078e2ba3b4db
Alf-e <ahillman02@hotmail.co.uk>
2023-10-27 18:50:37 +0100

    Added event listener for new room connect button

--- ClientServerApp/wwwroot/js/chat.js
+++ ClientServerApp/wwwroot/js/chat.js
@@ -6,22 +6,21 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 // Disable the send button until connection is established.
 document.getElementById("sendButton").disabled = true;
 
-connection.on("ReceiveMessage", function (user, message, port) {
+connection.on("ReceiveMessage", function (user, message, room) {
     // This method is called when a message is received from the server.
-    if(document.getElementById("portInput").value == port){
-        // Checks whether the received message is on the current port
+    console.log("Message received");
 
-        var li = document.createElement("li");
-        document.getElementById("messagesList").appendChild(li);
-        // Creates a new list item and adds it to the messagesList UL on index.cshtml
-        // We can assign user-supplied strings to an element's textContent because it
-        // is not interpreted as markup. If you're assigning in any other way, you 
-        // should be aware of possible script injection concerns.
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    // Creates a new list item and adds it to the messagesList UL on index.cshtml
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you 
+    // should be aware of possible script injection concerns.
 
-        li.textContent = `[PORT: ${port}]${user} says ${message}`;
-        // Assigns the list item content here
+    li.textContent = `[ROOM: ${room}]${user} says ${message}`;
+    // Assigns the list item content here
 
-    }
+
 });
 
 connection.start().then(function () {
@@ -33,13 +32,25 @@ connection.start().then(function () {
     // Prints any error message to console.
 });
 
+document.getElementById("roomButton").addEventListener("click", function (event) {
+    // This method is called when the room button is pressed.
+    var room = document.getElementById("roomInput").value;
+    var clientId = connection.clientId;
+
+    connection.invoke("RoomConnect", room).catch(function (err) {
+        // Calls the RoomConnect method in ChatHub.cs
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
 document.getElementById("sendButton").addEventListener("click", function (event) {
     // This method is called when the send button is pressed.
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
-    var port = document.getElementById("portInput").value;
-
-    connection.invoke("SendMessage", user, message, port).catch(function (err) {
+    var room = document.getElementById("roomInput").value;
+ 
+    connection.invoke("SendMessage", user, message, room).catch(function (err) {
         // Calls the SendMessage method in ChatHub.cs
         return console.error(err.toString());
     });
