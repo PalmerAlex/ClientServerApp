4522aa083f930d205b53494ad31023b209f1d661
Alf-e <ahillman02@hotmail.co.uk>
2023-11-07 14:47:02 +0000

    Scrapped RoomController concept. Just put its functionality in ChatHub

--- ClientServerApp/Hubs/ChatHub.cs
+++ ClientServerApp/Hubs/ChatHub.cs
@@ -7,69 +7,54 @@ namespace SignalRChat.Hubs
 {
     public class ChatHub : Hub
     {
-        private RoomController roomController;
+        private static Dictionary<string, string> roomConnections = new Dictionary<string, string>();
 
-        public ChatHub()
-        {
-            this.roomController = new(Groups);
-        }
         public async Task SendMessage(string user, string message, string room)
         {
             // This method gets called from chat.js when the send message button is pressed
 
-            string? targetRoomId = this.roomController.SearchRooms(Context.ConnectionId);
+            string? targetRoomId = await SearchRooms(Context.ConnectionId);
             // Finds the room the sender client is currently in
-           
+
             if (targetRoomId != null)
             {
                 // Only sends message if the target room has clients connected
-                
+
                 await Clients.Group(targetRoomId).SendAsync("ReceiveMessage", user, message, room);
                 // Calls ReceieveMessage on all the clients in the sender clients room
             }
-            
+
         }
 
         public async Task RoomConnect(string portId)
         {
             // This method gets called from chat.js when the connect room button is pressed
 
-            await roomController.AddClientToRoom(Context.ConnectionId, portId);
+            await AddClientToRoom(Context.ConnectionId, portId);
         }
 
-        private class RoomController
+
+        private async Task<IResult> AddClientToRoom(string clientId, string roomId)
         {
-            // This is an inner class that manages the SignalR Group functionality
-            private IGroupManager groupManager;
-            private static Dictionary<string,string> roomConnections = new Dictionary<string, string>();
-            
-            public RoomController(IGroupManager manager)
+            string? groupId = await SearchRooms(clientId);
+            if (groupId != null)
             {
-                this.groupManager = manager;
+                await Groups.RemoveFromGroupAsync(clientId, groupId);
+                roomConnections.Remove(clientId);
             }
-            public async Task<IResult> AddClientToRoom(string clientId, string roomId)
-            {
-                string? groupId = SearchRooms(clientId);
-                if(groupId != null){
-                    await groupManager.RemoveFromGroupAsync(clientId,groupId);
-                    roomConnections.Remove(clientId);
-                }
-                // Disconnect client from their current port before adding them to a new one
+            // Disconnect client from their current port before adding them to a new one
 
-                await groupManager.AddToGroupAsync(clientId,roomId);
-                roomConnections.Add(clientId,roomId);
-                // If a client tries to connect to a non-existant room, it creates one and adds them to it
-                return Results.Ok();
-            }
-
-            public string? SearchRooms(string targetClientId)
-            {
-                string? groupId;
-                roomConnections.TryGetValue(targetClientId, out groupId);
-                return groupId;
-            }
+            await Groups.AddToGroupAsync(clientId, roomId);
+            roomConnections.Add(clientId, roomId);
+            // If a client tries to connect to a non-existant room, it creates one and adds them to it
+            return Results.Ok();
         }
 
+        private async Task<string?> SearchRooms(string targetClientId)
+        {
+             string? groupId;
+            roomConnections.TryGetValue(targetClientId, out groupId);
+            return groupId;
+        }
     }
-
 }
